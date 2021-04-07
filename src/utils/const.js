b40a9952cf252aaee3dade77bbab5cd75b469aeb
craneink <craneink@163.com>
2021-04-07 11:54:24 +0800

    Update const.js

--- src/utils/const.js
+++ src/utils/const.js
@@ -49,8 +49,8 @@ export {
 export const AVATAR =
   'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU'; // Temp avatar
 export const NAVS = [
-  { text: 'Blog', link: 'https://github.com/yihong0618/running_page' },
-  { text: 'About', link: 'https://github.com/yihong0618/running_page' },
+  { text: '安贞桥偏北', link: 'https://www.moxuancn.com/' },
+
 ];
 
 const nike = 'rgb(224,237,94)';
